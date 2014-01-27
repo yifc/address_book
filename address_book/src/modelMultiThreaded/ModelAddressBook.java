586d4a715d19af1dbd5fbcc6ae735a03cce48d47
Julien Adam <julien.adam-dev@sfr.fr>
2014-01-27 09:51:04 +0100

    Multi-threaded implementation preparation

---
+++ address_book/src/modelMultiThreaded/ModelAddressBook.java
@@ -0,0 +1,265 @@
+package modelMultiThreaded;
+
+import java.awt.Desktop;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
+
+/**
+ * Class representing an address book with contacts
+ * @author Julien ADAM
+ * @version 1.0
+ * @see Observable
+ */
+public final class ModelAddressBook extends Observable {
+
+	/**
+	 * the list of contacts
+	 */
+	private List<ModelContact> book;
+	/**
+	 * the CSV filename where contacts are stored on filesystem
+	 */
+	private String filename;
+
+	/**
+	 * Constructs an address book
+	 * @param filename : the path to the CSV file where contacts are stored
+	 */
+	public ModelAddressBook(String filename){
+
+		String line;
+		book = new ArrayList<ModelContact>();
+		this.filename = filename;
+		try{ //File reading
+			BufferedReader br = new BufferedReader(new FileReader(filename));
+			while((line = br.readLine()) != null){
+				String[] tabArgs = line.split(",");
+				book.add(new ModelContact(tabArgs[0], tabArgs[1], tabArgs[2], tabArgs[3], tabArgs[4], tabArgs[5], tabArgs[6], tabArgs[7], tabArgs[8], tabArgs[9]));
+			}
+			br.close();
+		}
+		catch(Exception e){
+			System.out.println("Error : "+ e);
+		}
+		//Notify observer the list have been updated
+		toUpdate();
+	}
+
+	/**
+	 * Notifying function to the Observer objects, transmitting information that the address book have been updated
+	 */
+	public void toUpdate(){
+		setChanged();
+		notifyObservers();
+	}
+
+	@Override
+	public String toString() {
+		String chain = "";
+		for(int i = 0; i < book.size(); i++)
+			chain += " * " + book.get(i).toString() + "\n";
+		return chain;
+	}
+
+	/**
+	 * looks for a contact in the address book according to the contact last name
+	 * @param contact to find in address book
+	 * @return if succeeded, the list indice where contact is stored in database. Otherwise, -1
+	 */
+	public int findContact(ModelContact contact){
+		for(int i=0; i < book.size(); i++){
+			if(book.get(i).getLastName().compareTo(contact.getLastName()) == 0) return i;
+		}
+		return -1;
+	}
+
+	/**
+	 * Update a contact in the address book. If the contact is not found in the base, he is added instead.
+	 * @param contact : the contact to update
+	 */
+	public void updateContact(ModelContact contact){
+
+		int indice = this.findContact(contact);
+		if(indice < 0){
+			addContact(contact);
+			return;
+		}
+
+		book.set(indice, contact);
+		toUpdate();
+	}
+
+	/**
+	 * Delete given contact from the database.
+	 * @param contact : the contact to delete
+	 */
+	public void deleteContact(ModelContact contact){
+		int indice = this.findContact(contact);
+		if(indice < 0){
+			System.out.println("Contact " + contact.getLastName() + " " + contact.getFirstName() + " doesn't exist !");
+			return;
+		}
+		book.remove(indice);
+		toUpdate();
+	}
+
+	/**
+	 * Add a contact to the address book
+	 * @param contact : contact to add
+	 */
+	public void addContact(ModelContact contact){
+		book.add(contact);
+	}
+
+	/**
+	 * Reorder the address book to sort contacts per alphabetical order
+	 */
+	public void sortPerAlphabeticalOrder(){
+		Collections.sort(book, new Comparator<ModelContact>(){
+			public int compare(final ModelContact m1, final ModelContact m2){
+				return m1.getLastName().compareTo(m2.getLastName());
+			}
+		});
+		toUpdate();
+	}
+
+	/**
+	 * Reorder the address book to sort contacts per Last Updated, from the last updated to the first updated.
+	 */
+	public void sortPerLastUpdated(){
+		List<ModelContact> sorted = new ArrayList<ModelContact>(book);
+		Collections.sort(sorted, new Comparator<ModelContact>(){
+			public int compare(ModelContact m1, ModelContact m2) {
+				//reverse order : X (-1)
+				return (-1) * (m1.getUpdated().compareTo(m2.getUpdated()));
+			}
+		});
+		toUpdate();
+	}
+
+	/**
+	 * Get the number of contacts registered in the address book
+	 * @return the number of contacts
+	 */
+	public int getNbContacts(){
+		return book.size();
+	}
+
+	/**
+	 * Save the address book content in CSV file (the same as used to load address book)
+	 */
+	public void saveContact(){
+		try{ //file writing
+			BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
+			for(int i = 0; i < book.size(); i++)
+				bw.write(book.get(i).formatContact());
+			bw.flush();
+			bw.close();
+		}
+		catch(Exception e){
+			System.out.println("Error : "+ e);
+		}
+		toUpdate();
+	}
+
+	/**
+	 * Apply a filter in order to mark contact which have to be displayed according to the given group filter
+	 * @param group : group used to filter contacts. If group is "ALL", all contacts will be kept.
+	 */
+	public void filterByGroup(String group){
+		for(int i=0; i < book.size(); i++){
+			if(book.get(i).getGroup().compareTo(group) == 0 || group.compareTo("ALL") == 0){
+				book.get(i).setRequired(true);
+			}
+			else book.get(i).setRequired(false);
+		}
+		toUpdate();
+	}
+
+	/**
+	 * Re-initialization of group filter.
+	 */
+	public void filterByGroupInit(){
+		for(int i=0; i < book.size(); i++)
+			book.get(i).setRequired(false);
+	}
+
+	/**
+	 * Apply a filter in order to find the contact to display according to the id selected by the user
+	 * @param id : a chain constituted by last and first name from the selected contact 
+	 */
+	public void filterByContact(String id){
+		for(int i=0; i < book.size(); i++){
+			String chain = book.get(i).getLastName() + " " + book.get(i).getFirstName();
+			if(chain.compareTo(id) == 0){
+				book.get(i).setDisplayed(true);
+			}
+			else book.get(i).setDisplayed(false);
+		}
+		toUpdate();
+	}
+
+	/**
+	 * Re-initialization of contact filter
+	 */
+	public void filterByContactInit(){
+		for(int i=0; i < book.size(); i++)
+			book.get(i).setDisplayed(false);
+	}
+
+	public List<ModelContact> getBook() {
+		return book;
+	}
+
+	public void setBook(List<ModelContact> book) {
+		this.book = book;
+	}
+
+	/**
+	 * give the number of contact to keep after group filter application
+	 * @return : the number of contacts selected
+	 */
+	public int getNbRequired() {
+		int cpt=0;
+		for(int i= 0; i<book.size();i++)
+			if(book.get(i).isRequired())
+				cpt++;
+		return cpt;
+	}
+
+	/**
+	 * Open a web browser (if supported) and access to a specifing URL
+	 * @param url : url to access
+	 * @return True if browser opening succeeded. False otherwise.
+	 */
+	public boolean openHomePage(String url) {
+		try{
+			if(Desktop.isDesktopSupported() == true){
+				Desktop.getDesktop().browse(new URI("http://"+url));
+			}
+			else throw new IOException();
+		} catch( IOException | URISyntaxException e){
+			System.out.println("Trace : " + e);
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Open the mail client and start e-mail redaction for the given email address
+	 * @param mail : email to send an e-mail
+	 * @return : True if mail client opening succeeded. False otherwise.
+	 */
+	public boolean openEmail(String mail){
+		String url = "mailto:"+mail;
+		try {
+			Desktop.getDesktop().mail(new URI(url));
+		}catch( IOException | URISyntaxException e){
+			System.out.println("Trace : " + e);
+			return false;
+		}
+		return true;
+	}
+}
---
+++ address_book/src/modelMultiThreaded/ModelContact.java
@@ -0,0 +1,201 @@
+package modelMultiThreaded;
+
+import java.util.Date;
+
+/**
+ * Class representing a contact with all these fields
+ * @author Julien ADAM
+ * @version 1.0
+ */
+public final class ModelContact {
+	private String lastName;
+	private String firstName;
+	private String homeAddress;
+	private String companyAddress;
+	private String persPhone;
+	private String workPhone;
+	private String homePage;
+	private String email;
+	private String link;
+	private Date updated;
+	private String group;
+	private boolean isKept;
+	private boolean isDisplayed;
+	
+	/**
+	 * Construct a contact, according given values
+	 * @param group : its group
+	 * @param last : its last name (contacts unique key)
+	 * @param first : its first name
+	 * @param home : its home address
+	 * @param company : its company address
+	 * @param pers : its personal phone number
+	 * @param work : its company phone number
+	 * @param email : its e-mail address
+	 * @param homePage : its home page
+	 * @param link : its link
+	 */
+	public ModelContact(String group, String last, String first, String home, String company, String pers, String work, String email, String homePage, String link){
+		this.group = group;
+		lastName = last;
+		firstName = first;
+		homeAddress = home;
+		companyAddress = company;
+		persPhone = pers;
+		workPhone = work;
+		this.homePage = homePage;
+		this.email = email;
+		this.link = link;
+		updated = new Date();
+	}
+
+	public void setRequired(boolean isKept) {
+		this.isKept = isKept;
+	}
+	
+	public boolean isRequired() {
+		return isKept;
+	}
+	
+	public void setDisplayed(boolean display){
+		this.isDisplayed = display;
+	}
+	
+	public boolean isDisplayed(){
+		return isDisplayed;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result
+				+ ((homeAddress == null) ? 0 : homeAddress.hashCode());
+		result = prime * result
+				+ ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ModelContact other = (ModelContact) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (homeAddress == null) {
+			if (other.homeAddress != null)
+				return false;
+		} else if (!homeAddress.equals(other.homeAddress))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+	
+	public Date getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getHomeAddress() {
+		return homeAddress;
+	}
+	public void setHomeAddress(String homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+	public String getCompanyAddress() {
+		return companyAddress;
+	}
+	public void setCompanyAddress(String companyAddress) {
+		this.companyAddress = companyAddress;
+	}
+	public String getPersPhone() {
+		return persPhone;
+	}
+	public void setPersPhone(String persPhone) {
+		this.persPhone = persPhone;
+	}
+	public String getWorkPhone() {
+		return workPhone;
+	}
+	public void setWorkPhone(String workPhone) {
+		this.workPhone = workPhone;
+	}
+	public String getHomePage() {
+		return homePage;
+	}
+	public void setHomePage(String homePage) {
+		this.homePage = homePage;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	
+	public String toString(){
+		String chain = "Contact : " + firstName + " " + lastName
+				+ "\n\t- Home Address: " + homeAddress  
+				+ "\n\t- Company Address: " + companyAddress 
+				+ "\n\t- Personal Phone: " + persPhone 
+				+ "\n\t- Pro Phone : " + workPhone 
+				+ "\n\t- Home Page : " + homePage  
+				+ "\n\t- Email : " + email 
+				+ "\n\t- Link : " + link
+				+ "\n\t- Group : " + group
+				+ "\n\t- Last Updated : " + updated
+				+ "\n";
+		return chain;
+	}
+	
+	/**
+	 * Generate format line, required by CSV file format
+	 * @return : chain to insert info CSV file
+	 */
+	public String formatContact(){
+		return group+","+lastName+","+firstName+","+homeAddress+","+companyAddress+","+persPhone+","+workPhone+","+email+","+homePage+","+link+"\n";
+	}
+}
